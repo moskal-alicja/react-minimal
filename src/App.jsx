16c576c514c0dd9949083734348b07dd711abfc2
moskal alicja <moskala@p21710w.MINI>
2019-10-08 09:56:23 +0200

    Production bundle analysis

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const MyApp = () => (
   <h1>Minimal React moskal-alicja c70dff912fa85f3a1951d0b4f9ec4460e2cf1f0b</h1>  
+  <p>Bundle size: 40.7 KB, Load time of the bundle: 522 ms, Last commit SHA1: c70dff912fa85f3a1951d0b4f9ec4460e2cf1f0b</p>
 )
 
 export default MyApp
