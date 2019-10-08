8a41e3d947fac312e7951e8509386bbba1a87d58
moskal alicja <moskala@p21710w.MINI>
2019-10-08 09:47:17 +0200

    Customize headline

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MyApp = () => (
-  <h1>Minimal React</h1>  
+  <h1>Minimal React moskal-alicja c70dff912fa85f3a1951d0b4f9ec4460e2cf1f0b</h1>  
 )
 
 export default MyApp
