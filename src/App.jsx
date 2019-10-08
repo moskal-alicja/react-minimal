1f72f7ec76b4b4ad4f4a5e2046e5335e1bc04d54
moskal alicja <moskala@p21710w.MINI>
2019-10-08 10:07:31 +0200

    Production bundle analysis new data

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MyApp = () => (
   <div>
   <h1>Minimal React moskal-alicja c70dff912fa85f3a1951d0b4f9ec4460e2cf1f0b</h1>  
-  <p>Bundle size: 40.7 KB, Load time of the bundle: 522 ms, Last commit SHA1: c70dff912fa85f3a1951d0b4f9ec4460e2cf1f0b</p>
+  <p>Bundle size: 60 bytes, Load time of the bundle: 35 ms, Last commit SHA1: 8fbe21026f44466cee137acb38fdc3334bf43c0c</p>
   </div>
 )
 
