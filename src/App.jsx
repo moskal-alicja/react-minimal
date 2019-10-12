ab2b8cab997bcfb341ed5ec294e76b3d62ccd5d0
moskal-alicja <moskal.ala@gmail.com>
2019-10-12 22:56:03 +0200

    Lab1Ex3

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,12 @@ const MyApp = () => (
   <AppTitle />
   <p>Bundle size: 60 bytes, Load time of the bundle: 35 ms, Last commit SHA1: 8fbe21026f44466cee137acb38fdc3334bf43c0c</p>
   <div>generateArray with n = 10: {generateArray(10).toString()}</div>
+  <div>generateRandomArray with n = 10: {generateRandomArray(10).toString()}</div>
   </div>
 )
 
 const generateArray = (n) => Array(n).fill().map((x,i) => i + 1)
+
+const generateRandomArray = (n) => Array(n).fill().map(() => 1 + Math.floor(Math.random() * 25))
 
 export default MyApp
