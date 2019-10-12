6dec17f66d0d4467709b13504501bb969a908c60
moskal-alicja <moskal.ala@gmail.com>
2019-10-12 20:06:42 +0200

    Lab1Ex1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import AppTitle from './AppTitle'
 
 const MyApp = () => (
   <div>
-  <h1>Minimal React moskal-alicja c70dff912fa85f3a1951d0b4f9ec4460e2cf1f0b</h1>  
+  <AppTitle />
   <p>Bundle size: 60 bytes, Load time of the bundle: 35 ms, Last commit SHA1: 8fbe21026f44466cee137acb38fdc3334bf43c0c</p>
   </div>
 )
+
 
 export default MyApp
---
+++ src/AppTitle.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const AppTitle = () => (
+  <h1>Minimal React moskal-alicja</h1>  
+)
+
+export default AppTitle
