ceeae48b700913f862790d9667d47234920ee696
moskal-alicja <moskal.ala@gmail.com>
2019-10-18 20:09:34 +0200

    Lab2Ex2

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import AppTitle from './AppTitle'
 import Students from './Students'
-import InputFields from './DynamicArray'
+import DynamicArray from './DynamicArray'
 
 
 const MyApp = () => (
   <div>
-  <AppTitle />
-  <p>Bundle size: 60 bytes, Load time of the bundle: 35 ms, Last commit SHA1: 8fbe21026f44466cee137acb38fdc3334bf43c0c</p>
-  <div>generateArray with n = 10: {generateArray(10).toString()}</div>
-  <div>generateRandomArray with n = 10: {generateRandomArray(10).toString()}</div>
-  <div>Elements from [{constArray.toString()}] larger than 15: {constArray.filter(x => x > 15).toString()}</div>
-  <div>Elements from [{MyArray.toString()}] to their square root: [{squareRootFromMyArray.call().toString()}]</div>
-  <p>JSON data: </p>
-  <Students />
-
-  <h3>LAB 2</h3>
-  <InputFields />
-
+    <AppTitle />
+    <p>Bundle size: 60 bytes, Load time of the bundle: 35 ms, Last commit SHA1: 8fbe21026f44466cee137acb38fdc3334bf43c0c</p>
+    <div>generateArray with n = 10: {generateArray(10).toString()}</div>
+    <div>generateRandomArray with n = 10: {generateRandomArray(10).toString()}</div>
+    <div>Elements from [{constArray.toString()}] larger than 15: {constArray.filter(x => x > 15).toString()}</div>
+    <div>Elements from [{MyArray.toString()}] to their square root: [{squareRootFromMyArray.call().toString()}]</div>
+    <p>JSON data: </p>
+    <Students />
+    <div className="lab2">
+      <h3>LAB 2</h3>
+      <p>To display an array insert {"0 < a < b"}</p>
+      <DynamicArray />
+    </div>   
   </div>
 )
 
--- src/DynamicArray.jsx
+++ src/DynamicArray.jsx
@@ -1,31 +1,56 @@
 import React from 'react'
 
 
-class InputFields extends React.Component {
+class DynamicArray extends React.Component {
     constructor() {
         super();
         this.state = {
             a: 0,
-            b: 0
+            b: 0, 
+            array: []
         }
 
         this.inputChanged = this.inputChanged.bind(this);     
     }
 
+    generateArray = (a, b) => Array(b - a + 1).fill().map((x,i) => a + i)
+
     inputChanged(event) {
-        console.log(`Value changed ${event.target.name}: ${event.target.value}`);
-        this.setState({[event.target.name]: event.target.value});
+        console.log(`Value changed ${event.target.name}: ${event.target.value}`); 
+        const val = parseInt(event.target.value);
+        let arr = [];
+        if(event.target.name == "a") {
+            if(val < this.state.b && val > 0) {
+                arr = this.generateArray(val, this.state.b);
+            }
+        }
+        else {
+            if(val > this.state.a && this.state.a > 0) {
+                arr = this.generateArray(this.state.a, val);
+            }
+        }            
+        this.setState({
+            [event.target.name]: val, 
+            array: arr
+        });
     } 
 
     render() {
         return(
-            <div className="inputFields">
-                a: <input type="number" name="a" onChange={this.inputChanged}></input> 
-                b: <input type="number" name="b" onChange={this.inputChanged}></input>
-            </div>
-
+            <div className="dynamicArray">
+                <div className="inputFields">
+                    a: <input type="number" name="a" onChange={this.inputChanged}></input> 
+                    b: <input type="number" name="b" onChange={this.inputChanged}></input>
+                </div>
+                { this.state.array.length > 0 &&
+                <p>
+                    Generated array: 
+                    {this.state.array.toString()}
+                </p>
+                }
+            </div>           
         );
     }  
 }
 
-export default InputFields
+export default DynamicArray
